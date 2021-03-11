76efb714344dd56f884c30d1409b0fc1238c4530
Blacked1234 <72291767+Blacked1234@users.noreply.github.com>
2021-03-11 17:19:26 +0100

    Update main.cpp

--- Template/src/main.cpp
+++ Template/src/main.cpp
@@ -1,7 +1,86 @@
 #include <iostream>
+using namespace std;
+
+class Square
+{
+private:
+    double dlugosc{};
+public:
+    Square();
+    ~Square();
+
+    Square(double dlugosc)
+    {
+        this->dlugosc = dlugosc;
+    }
+
+    static void pole(double dlugosc);
+
+    static void obwod(double dlugosc);
+
+};
+
+void Square::pole(double dlugosc)
+{
+    double pole = dlugosc * dlugosc;
+    cout << pole << endl;
+}
+
+void Square::obwod(double dlugosc)
+{
+    double obwod = dlugosc * 4;
+    cout << obwod << endl;
+}
+
+
+Square::Square() {
+
+}
+
+class Circle
+{
+private:
+    double promien;
+public:
+    Circle();
+    ~Circle();
+
+    Circle(double promien)
+    {
+        this->promien = promien;
+    }
+
+    static void pole(double promien);
+    static void obwod(double promien);
+
+};
+
+Circle::Circle() {
+
+}
+
+void Circle::pole(double promien) {
+    double pole = 3.14*promien*promien;
+    cout << pole << endl;
+}
+
+void Circle::obwod(double promien) {
+    double obwod = 2*3.14*promien;
+    cout << obwod << endl;
+}
 
 int main(int argc, char *argv[])
 {
-    std::cout << "Hello world." << std::endl;
-    return 0;
+
+    Square *kwadrat = new Square();
+
+    kwadrat->pole(14);
+    kwadrat->obwod(14);
+
+    Circle *kolo = new Circle();
+
+    kolo->pole(5);
+    kolo->obwod(5);
+
+
 }
